a2c87055bae1e05c0b5d389f4815ab41d55eb19e
Nicholas Monath <nmonath@cs.umass.edu>
2020-04-18 12:02:30 -0400

    pip installable package

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mysql2tsv',
+      version='0.01',
+      packages=['mysql2tsv'],
+      package_dir={'mysql2tsv': 'mysql2tsv'}
+)
